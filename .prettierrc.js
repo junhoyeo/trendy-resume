42d5c67c47d4d3aa2e8239d2e45da281652df76e
Junho Yeo <i@junho.io>
2022-07-13 10:34:19 +0900

    chore: update Prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+
+  // @ianvs/prettier-plugin-sort-imports
+  importOrder: ['<THIRD_PARTY_MODULES>', '@/(.*)$', '^[./](.*)$'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: [require('@ianvs/prettier-plugin-sort-imports')],
+};
