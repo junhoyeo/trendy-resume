bde30f3184ff17554dee03424988ffd403a5a43f
Junho Yeo <i@junho.io>
2022-07-13 10:23:28 +0900

    fix: rewrite `CustomApp` as function component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import App from 'next/app';
+import { AppProps } from 'next/app';
 import Head from 'next/head';
 
 import '../styles/global.scss';
 
-type AppProps = {
-  Component: React.ReactNode,
-  pageProps: any;
-};
-
-export default class CustomApp extends App<AppProps> {
-  public render() {
-    const { Component, pageProps } = this.props;
-
-    return (
-      <>
-        <Head>
-          <title>Résumé: Junhoyeo</title>
-          <meta name="description" content="Frontend developer Junhoyeo's Résumé" />
-        </Head>
-        <Component {...pageProps} />
-      </>
-    );
-  }
+export default function CustomApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Résumé: Junhoyeo</title>
+        <meta name="description" content="Frontend developer Junhoyeo's Résumé" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
