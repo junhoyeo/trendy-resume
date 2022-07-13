fef187e1c6ba71b9577681de9fd0a2a76f38d6e0
Junho Yeo <i@junho.io>
2022-07-13 10:59:57 +0900

    fix: implement `NoSSR` to fix hydration mismatch

--- src/components/templates/ProductSection.tsx
+++ src/components/templates/ProductSection.tsx
@@ -1,9 +1,10 @@
-import * as React from 'react';
+import React, { useMemo } from 'react';
 import styled from 'styled-components';
 
 import { IProduct } from '@/utils/types';
 import useWindowSize from '@/utils/useWindowSize';
 
+import { NoSSR } from '../NoSSR';
 import Section from '../atoms/Section';
 import { TitleText } from '../atoms/Text';
 import ProductCard from '../organisms/ProductCard';
@@ -15,19 +16,27 @@ type ProductSectionProps = {
   products: IProduct[];
 };
 
+const getAlignment = (
+  products: IProduct[],
+  width: number,
+): [number, number] => {
+  if (width < 400) {
+    return [0, 0];
+  }
+  const cardsInOneRow = width <= 950 ? 2 : 4;
+  return [products.length % cardsInOneRow, cardsInOneRow];
+};
+
 export const ProductSection: React.FC<ProductSectionProps> = ({
   id = '',
   title,
   products,
 }) => {
   const { width = 0 } = useWindowSize();
-  const [lack, cardsInOneRow] = ((width: number): [number, number] => {
-    if (width < 400) {
-      return [0, 0];
-    }
-    const cardsInOneRow = width <= 950 ? 2 : 4;
-    return [products.length % cardsInOneRow, cardsInOneRow];
-  })(width);
+  const [lack, cardsInOneRow] = useMemo(
+    () => getAlignment(products, width),
+    [products, width],
+  );
 
   return (
     <Section id={id}>
@@ -58,10 +67,13 @@ export const ProductSection: React.FC<ProductSectionProps> = ({
             />
           );
         })}
-        {!lack ||
-          [...Array(cardsInOneRow - lack)].map((_, idx: number) => (
-            <SkeletonCard key={`skeleton-${idx}`} />
-          ))}
+
+        <NoSSR>
+          {!!lack &&
+            [...Array(cardsInOneRow - lack)].map((_, idx: number) => (
+              <SkeletonCard key={`skeleton-${idx}`} />
+            ))}
+        </NoSSR>
       </ProductList>
     </Section>
   );
